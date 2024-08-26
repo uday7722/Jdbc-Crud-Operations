afc4085d1b90fb07eea26dfa2f0fd3e87c48c320
Uday Annapureddy <142037019+uday7722@users.noreply.github.com>
2024-08-26 10:49:07 +0530

    Delete OneDrive/Desktop/FinalProject/Online-Shopping-Application directory

--- OneDrive/Desktop/FinalProject/Online-Shopping-Application/src/main/java/com/jsp/repo/SellerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.model.Seller;
-
-public interface SellerRepo extends JpaRepository<Seller , Integer> {
-
-}
